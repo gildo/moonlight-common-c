ef9085db05dd0a42eacb29a2a108fbce6f1207f2
Ermenegildo Fiorito <gildo.fiorito@gmail.com>
2026-06-08 10:28:21 +0200

    Increase Xbox/Sunshine maximum bitrate cap to 150 Mbps

--- src/SdpGenerator.c
+++ src/SdpGenerator.c
@@ -358,7 +358,8 @@ static PSDP_OPTION getAttributesList(char*urlSafeAddr) {
     // GFE currently imposes a limit of 100 Mbps for the video bitrate. It will automatically
     // impose that on maximumBitrateKbps but not on initialBitrateKbps. We will impose the cap
     // ourselves so initialBitrateKbps does not exceed maximumBitrateKbps.
-    adjustedBitrate = adjustedBitrate > 100000 ? 100000 : adjustedBitrate;
+    // For Xbox/Sunshine, we increase this cap to 150 Mbps to allow for ultra high quality streaming.
+    adjustedBitrate = adjustedBitrate > 150000 ? 150000 : adjustedBitrate;
 
     // We don't support dynamic bitrate scaling properly (it tends to bounce between min and max and never
     // settle on the optimal bitrate if it's somewhere in the middle), so we'll just latch the bitrate
